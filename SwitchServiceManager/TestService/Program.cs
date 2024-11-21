a9c51c68f83dead75d633aae56b70ce61e7685f5
serhiy-petryk <sergei_petrik@yahoo.com>
2024-11-21 23:33:16 +0200

    added TestService project

---
+++ SwitchServiceManager/TestService/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.ServiceProcess;
+
+namespace TestService
+{
+    static class Program
+    {
+        // https://learn.microsoft.com/en-us/dotnet/framework/windows-services/walkthrough-creating-a-windows-service-application-in-the-component-designer
+        // https://www.c-sharpcorner.com/article/create-windows-services-in-c-sharp/
+        //Install:      I:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe E:\Apps\project-2023\SetSwitchServiceDate\SwitchServiceManager\TestService\bin\Debug\TestService.exe
+        //Uninstall:    I:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe -u E:\Apps\project-2023\SetSwitchServiceDate\SwitchServiceManager\TestService\bin\Debug\TestService.exe
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new TestService()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
---
+++ SwitchServiceManager/TestService/TestService.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.ServiceProcess;
+
+namespace TestService
+{
+    public partial class TestService : ServiceBase
+    {
+        private const string LogFileName = "E:\\Temp\\TestServiceLog.txt";
+
+        public TestService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            // Update the service state to Start Pending.
+            var serviceStatus = new ServiceStatus
+            {
+                dwCurrentState = ServiceState.SERVICE_START_PENDING,
+                dwWaitHint = 100000
+            };
+            StatusHelper.SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+
+            WriteToLog($"Started");
+
+            // Update the service state to Running.
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_RUNNING;
+            StatusHelper.SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+
+        }
+
+        protected override void OnStop()
+        {
+            // Update the service state to Stop Pending.
+            var serviceStatus = new ServiceStatus
+            {
+                dwCurrentState = ServiceState.SERVICE_STOP_PENDING,
+                dwWaitHint = 100000
+            };
+            StatusHelper.SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+
+            WriteToLog($"Stopped");
+
+            // Update the service state to Stopped.
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_STOPPED;
+            StatusHelper.SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+        }
+
+        protected override void OnShutdown()
+        {
+            // Update the service state to Stop Pending.
+            var serviceStatus = new ServiceStatus
+            {
+                dwCurrentState = ServiceState.SERVICE_STOP_PENDING,
+                dwWaitHint = 100000
+            };
+            StatusHelper.SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+
+            WriteToLog($"Shutdown");
+            base.OnShutdown();
+
+            // Update the service state to Stopped.
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_STOPPED;
+            StatusHelper.SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+        }
+
+        public void WriteToLog(string message) => File.AppendAllText(LogFileName, $"TestService: {message} {DateTime.Now:O} {IsAdministrator()}" + Environment.NewLine);
+
+        public static string IsAdministrator()
+        {
+            using (var identity = WindowsIdentity.GetCurrent())
+            {
+                var principal = new WindowsPrincipal(identity);
+                return principal.IsInRole(WindowsBuiltInRole.Administrator) ? "Admin" : "No admin";
+            }
+        }
+
+        //============================================
+        //============================================
+        //============================================
+        public enum ServiceState
+        {
+            SERVICE_STOPPED = 0x00000001,
+            SERVICE_START_PENDING = 0x00000002,
+            SERVICE_STOP_PENDING = 0x00000003,
+            SERVICE_RUNNING = 0x00000004,
+            SERVICE_CONTINUE_PENDING = 0x00000005,
+            SERVICE_PAUSE_PENDING = 0x00000006,
+            SERVICE_PAUSED = 0x00000007,
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct ServiceStatus
+        {
+            public int dwServiceType;
+            public ServiceState dwCurrentState;
+            public int dwControlsAccepted;
+            public int dwWin32ExitCode;
+            public int dwServiceSpecificExitCode;
+            public int dwCheckPoint;
+            public int dwWaitHint;
+        };
+        public class StatusHelper
+        {
+            [DllImport("advapi32.dll", SetLastError = true)]
+            public static extern bool SetServiceStatus(System.IntPtr handle, ref ServiceStatus serviceStatus);
+        }
+
+    }
+}
