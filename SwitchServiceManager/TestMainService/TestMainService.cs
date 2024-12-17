4dc4f72071760db97eccc44b198316eb3d42b749
serhiy-petryk <sergei_petrik@yahoo.com>
2024-12-17 10:25:49 +0200

    x

--- SwitchServiceManager/TestMainService/TestMainService.cs
+++ SwitchServiceManager/TestMainService/TestMainService.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Security.Principal;
 using System.ServiceProcess;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace TestMainService
 {
@@ -21,58 +23,76 @@ namespace TestMainService
 
         protected override void OnStart(string[] args)
         {
+            WriteToLog($"{Environment.NewLine}");
+
             if (args.Length == 0)
             {
                 var iniFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "args.ini");
                 var argsExists = File.Exists(iniFilePath);
                 args = File.ReadAllLines(iniFilePath).Where(a => !a.StartsWith("#")).ToArray();
             }
+            var argString = string.Join(" ", args);
+
+            // Find child service
+            var childService = args.Length > 0 ? GetService(args[0]) : null;
+            var childServiceStatus = childService?.Status.ToString() ?? "No service";
+            WriteToLog($"Started, args: {argString}; service status: {childServiceStatus}");
+            if (childService == null) return;
+
+            DateTime? atDate = null;
+            if (args.Length > 1)
+            {
+                var dateOk = DateTime.TryParseExact(args[1], "yyyy-MM-dd", null, DateTimeStyles.None, out var date2);
+                if (dateOk) atDate = date2;
+            }
+
+            var dayDiff = atDate.HasValue ? Convert.ToInt32((DateTime.UtcNow - atDate.Value).TotalDays) : (int?)null;
 
             // Update the service state to Start Pending.
             var serviceStatus = new ServiceStatus
             {
                 dwCurrentState = ServiceState.SERVICE_START_PENDING,
-                dwWaitHint = 100000
+                dwWaitHint = 20000
             };
             StatusHelper.SetServiceStatus(this.ServiceHandle, ref serviceStatus);
-
-            var argString = string.Join(" ", args);
-            // WriteToLog($"Started, {AppDomain.CurrentDomain.BaseDirectory} {Environment.CurrentDirectory} {System.Reflection.Assembly.GetEntryAssembly().Location} {Process.GetCurrentProcess().MainModule.FileName} {argsExists} args: {argString}");
-            var childService = args.Length > 0 ? GetService(args[0]) : null;
-            var childServiceStatus = childService?.Status.ToString() ?? "No service";
-            WriteToLog($"Started, args: {argString}; service status: {childServiceStatus}");
-
-            if (childService != null && childService.Status == ServiceControllerStatus.Stopped)
-            {
-                DateTime? date = null;
-                if (args.Length > 1)
-                {
-                    var dateOk =
-                        DateTime.TryParseExact(args[2], "yyyy-MM-dd", null, DateTimeStyles.None, out var date2);
-                    if (dateOk) date = date2;
-                }
-
-                childService.Start();
-                childService.WaitForStatus(ServiceControllerStatus.Running);
-                // System.Threading.Thread.Sleep(5000);
-                WriteToLog($"Service {childService.ServiceName} started");
-            }
 
             // Update the service state to Running.
             WriteToLog($"On Start: BeforeEnd");
             serviceStatus.dwCurrentState = ServiceState.SERVICE_RUNNING;
             StatusHelper.SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+
+            Task.Run(() =>
+            {
+                WriteToLog($"Task.Run. Status: {childService.Status}");
+                if (childService.Status == ServiceControllerStatus.Stopped)
+                {
+                    // if (childService.StartType != ServiceStartMode.Automatic)
+                    {
+                        childService.Start();
+                    }
+
+                    childService.WaitForStatus(ServiceControllerStatus.Running);
+                    WriteToLog($"Service {childService.ServiceName} started. Day difF: {dayDiff}");
+                }
+                WriteToLog($"Task.Run. End code. Status {childService.Status}");
+            });
+
             WriteToLog($"On Start: AfterEnd");
         }
 
-        protected override void OnStop()
+        /*protected override void OnStop()
         {
-        }
+        }*/
 
         private void WriteToLog(string message)
         {
             if (Directory.Exists(Path.GetDirectoryName(LogFileName)))
-                File.AppendAllText(LogFileName, $"TestMainService: {message}; {DateTime.Now:O} {IsAdministrator()}" + Environment.NewLine);
+            {
+                if (string.IsNullOrEmpty(message.Trim()))
+                    File.AppendAllText(LogFileName, message);
+                else
+                    File.AppendAllText(LogFileName, $"TestMainService: {message}; {DateTime.Now:O} {IsAdministrator()}" + Environment.NewLine);
+            }
         }
 
         private static string IsAdministrator()
@@ -118,5 +138,37 @@ namespace TestMainService
             public static extern bool SetServiceStatus(System.IntPtr handle, ref ServiceStatus serviceStatus);
         }
 
+        //==============================
+        //==============================
+        //==============================
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SYSTEMTIME
+        {
+            public ushort Year;
+            public ushort Month;
+            public ushort DayOfWeek;
+            public ushort Day;
+            public ushort Hour;
+            public ushort Minute;
+            public ushort Second;
+            public ushort Milliseconds;
+
+            public SYSTEMTIME(DateTime dt)
+            {
+                Year = (ushort)dt.Year;
+                Month = (ushort)dt.Month;
+                DayOfWeek = (ushort)dt.DayOfWeek;
+                Day = (ushort)dt.Day;
+                Hour = (ushort)dt.Hour;
+                Minute = (ushort)dt.Minute;
+                Second = (ushort)dt.Second;
+                Milliseconds = (ushort)dt.Millisecond;
+            }
+        }
+
+        // SYSTEMTIME systime = new SYSTEMTIME(date);
+        // SetSystemTime(ref systime);
+        [DllImport("kernel32.dll")]
+        static extern bool SetSystemTime(ref SYSTEMTIME time);
     }
 }
